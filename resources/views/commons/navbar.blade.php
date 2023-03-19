ebe0204e072e52d8940d9c267d62c757956531be
hayato.yoshimura <tsumarue@gmail.com>
2023-03-19 09:19:33 +0000

    CRUD tasklist

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -2,13 +2,13 @@
     <nav class="navbar bg-neutral text-neutral-content">
         {{-- トップページへのリンク --}}
         <div class="flex-1">
-            <h1><a class="btn btn-ghost normal-case text-xl" href="/">Taslist</a></h1>
+            <h1><a class="btn btn-ghost normal-case text-xl" href="/">Tasklist</a></h1>
         </div>
 
         <div class="flex-none">
             <ul tabindex="0" class="menu lg:block lg:menu-horizontal">
                 {{-- メッセージ作成ページへのリンク --}}
-                <li><a class="link link-hover" href="{{ route('messages.create') }}">新規タスクの投稿</a></li>
+                <li><a class="link link-hover" href="{{ route('tasks.create') }}">新規タスクの投稿</a></li>
             </ul>
         </div>
     </nav>
